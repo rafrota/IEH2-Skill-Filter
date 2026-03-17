6306bd5bf642729c31a7087a1df58a78645d49b6
rafrota <70604821+rafrota@users.noreply.github.com>
2026-03-17 15:30:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 def get_table(json_data, frame=st):
     df = pd.DataFrame(json_data)
+    pd.set_option('display.max_colwidth', None)
     if df.empty:
         frame.text("No data found!")
         return
@@ -16,6 +17,7 @@ def get_table(json_data, frame=st):
         df.style.pipe(make_pretty),
         # df.style.apply(operacao_color, axis=0),
         width="stretch",
+        height="stretch",
         # For `use_container_width=True`, use `width='stretch'`. For `use_container_width=False`, use `width='content'`.
         hide_index=True,
         column_order=["class", "skill", "lv", "effect"],
@@ -31,7 +33,7 @@ def get_table(json_data, frame=st):
             ),
             "lv": st.column_config.TextColumn(
                 "lv",
-                width="small"
+                width="large"
             ),
             "effect": st.column_config.TextColumn(
                 "Effect",
@@ -56,15 +58,28 @@ def get_all_from_file():
                     })
     return all_data
 
-def find_effect(hero_class, effect):
+def find_effect(hero_class, group_skill, effect):
     all_data = st.session_state.all_data
     filtered_data = []
     for hero_info in all_data:
         print(hero_info)
         if hero_class.lower() == hero_info["class"] or hero_class == "All":
             if effect in hero_info["effect"]:
-                filtered_data.append(hero_info)
+                finded = False
+                if group_skill:
+                    for data in filtered_data:
+                        if data["skill"] == hero_info["skill"]:
+                            finded = True
+                            if ' ~~ ' in data["effect"]:
+                                data["effect"] = f'{data["effect"].split(" ~~ ")[0]} ~~ {hero_info["effect"]}'
+                                data["lv"] = f'{data["lv"].split(" ~~ ")[0]} ~~ {hero_info["lv"]}'
+                            else:
+                                data["effect"] = f'{data["effect"]} ~~ {hero_info["effect"]}'
+                                data["lv"] = f'{data["lv"]} ~~ {hero_info["lv"]}'
+                if not finded:
+                    filtered_data.append(hero_info)
     return filtered_data
+
 
 st.set_page_config(
     page_title="Idle Epic Hero 2 Filter",
@@ -76,5 +91,7 @@ st.title("Idle Epic Hero 2 Filter")
 all_data = st.session_state.all_data
 hero_class = st.selectbox("Class", ["All","Warrior","Wizard","Angel","Thief","Archer","Tamer"])
 input = st.text_input('Filtro')
-retorno = find_effect(hero_class, input)
+group_skill = st.checkbox('Group Skill')
+retorno = find_effect(hero_class, group_skill, input)
+
 get_table(retorno)
